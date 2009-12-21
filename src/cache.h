96f779c8a47d9c3b2c0966cb74becc0e7d955a63
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2009-12-21 18:35:36 -0500

    Remembered that local variables go in the scene (or a spare variable in the main loop, or something) and global variables are the only ones in the cache. COND command still doesn't work correctly, though.

--- src/cache.h
+++ src/cache.h
@@ -34,11 +34,9 @@ typedef struct {
 } CACHE_LOG_ENTRY;
 
 typedef struct {
-   CACHE_VARIABLE* globals, /* Global variables. */
-      * locals; /* Local variables. */
+   CACHE_VARIABLE* globals; /* Global variables. */
    CACHE_LOG_ENTRY* text_log; /* Log of previously displayed text. */
    int globals_count,
-      locals_count,
       text_log_count, /* Number of previously spoken text item. */
       player_team_count, /* Number of members in the player's party. */
       player_team_front; /* The team member currently playable. */
@@ -49,10 +47,38 @@ typedef struct {
 
 /* = Macros = */
 
+/* Purpose: Determine the scope and set a convenience pointer to the correct  *
+ *          list in the given cache for that scope.                           */
+/* Parameters: The convenience pointer, the pointer to the scope's variable   *
+ *             count, the pointer to the cache, and the requested scope.      */
+#define CACHE_SCOPE_LOAD( array_ptr, counter_ptr, cache_ptr, scope ) \
+   if( COND_SCOPE_GLOBAL == scope ) { \
+      array_ptr = cache_ptr->globals; \
+      counter_ptr = &cache_ptr->globals_count; \
+   } else if( COND_SCOPE_LOCAL == scope ) { \
+      array_ptr = ps_cache_in->locals; \
+      counter_ptr = &cache_ptr->locals_count; \
+   } else { \
+      DBG_ERR_INT( "Unable to Load; Unknown variable scope", scope ); \
+   }
+
+/* Purpose: Save the convenience pointer given earlier back to the correct    *
+ *          scope in the given cache.                                         */
+/* Parameters: The convenience pointer, the pointer to the cache, and the     *
+ *             requested scope.                                               */
+#define CACHE_SCOPE_SAVE( array_ptr, cache_ptr, scope ) \
+   if( COND_SCOPE_GLOBAL == scope ) { \
+      cache_ptr->globals = array_ptr; \
+   } else if( COND_SCOPE_LOCAL == scope ) { \
+      ps_cache_in->locals = array_ptr; \
+   } else { \
+      DBG_ERR_INT( "Unable to save; Unknown variable scope", scope ); \
+   }
+
 /* = Function Prototypes = */
 
 BOOL cache_save( bstring );
 BOOL cache_load( bstring );
-void cache_set_var( bstring, bstring, COND_SCOPE, CACHE_CACHE* );
+CACHE_VARIABLE* cache_set_var( bstring, bstring, CACHE_VARIABLE*, int* );
 
 #endif /* CACHE_H */
